b8557c62f7445ff765e44d58bb490ce4533e0d9c
q123q123q <43091702+q123q123q@users.noreply.github.com>
2018-09-08 21:04:07 +0800

    Create vsplatego.sh

---
+++ vsplatego.sh
@@ -0,0 +1,6 @@
+＃！/斌/庆典
+
+service mysqld start || 服务启动mysql &&的mysql -u根-ptoor -e '创建数据库ECSHOP; ' ;
+mkdir / var / www / html / tmp ;
+wget的-O /var/www/html/tmp/ECShop_3.6.0_UTF8_installed.zip http://test-1251457471.cosgz.myqcloud.com/ECShop_3.6.0_UTF8_vuln_Installed.zip &&解压-d在/ var / www / html等/ tmp目录/var/www/html/tmp/ECShop_3.6.0_UTF8_installed.zip && MV /var/www/html/tmp/ECShop_3.6.0_UTF8_installed/source/ *的/ var / www / html等/ && RM -rf /无功/网络/ html / tmp ;
+chmod -R 777 / var / www / html ;
